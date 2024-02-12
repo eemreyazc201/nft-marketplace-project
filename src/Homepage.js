efe2a9286a8839d2e30c6e06751473651d023951
barisbice1 <barbicebeyi@gmail.com>
2024-02-12 15:49:14 +0300

    commit

---
+++ src/Homepage.js
@@ -0,0 +1,124 @@
+// Homepage.js
+
+import React from "react";
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="container mt-6">
+      <h2 className="is-size-2 has-text-centered mb-4">Top Picks</h2>
+      <div className="columns is-multiline is-centered">
+        {/* NFT 1 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft2.png" alt="NFT 1" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 1</p>
+              <p className="price">Price: $100</p>
+              <p className="highest-bid">Highest Bid: $150</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+
+        {/* NFT 2 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft3.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+        {/* NFT 2 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft4.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+        {/* NFT 2 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft5.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+
+        {/* NFT 2 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft6.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+
+        {/* NFT 2 */}
+        <div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft7.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+
+{/* NFT 2 */}
+<div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft11.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+
+{/* NFT 2 */}
+<div className="column is-one-quarter">
+          <div className="image-container">
+            <img src="/images/nft3.png" alt="NFT 2" className="image" />
+            <div className="info">
+              <p className="nft-name">NFT Name 2</p>
+              <p className="price">Price: $120</p>
+              <p className="highest-bid">Highest Bid: $180</p>
+              {/* Add more information as needed */}
+            </div>
+          </div>
+        </div>
+{/* NFT 2 */}
+
+
+
+
+        {/* Continue the pattern for NFT 3, NFT 4, etc. */}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
+
