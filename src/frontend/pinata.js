f287235c1536e302103837c3e7acbb5461c9138d
Mehmet Akif <dundarm22@itu.edu.tr>
2024-02-18 16:31:51 +0300

    uploadToIPFS

---
+++ src/frontend/pinata.js
@@ -0,0 +1,36 @@
+const apiKey = 'PINATA_API_KEY';  // Buralara sonradan ekleme yapılacak.
+const apiSecret = 'PINATA_API_SECRET';
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadFileToIPFS = async (file) => {   
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+
+    let data = new FormData();
+    data.append('file', file);
+
+    return axios
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: apiKey, 
+                pinata_secret_api_key: apiSecret,  
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash);
+            return {
+                success: true,
+                pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+            };
+        })
+        .catch(function (error) {
+            console.log(error);
+            return {
+                success: false,
+                message: error.message,
+            };
+        });
+};
